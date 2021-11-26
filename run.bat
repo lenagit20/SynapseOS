6d6c413d11c43e45069f777956674f7e6907c3f2
Aren Elchinyn <slizen.can@gmail.com>
2021-11-26 20:02:32 +0300

    Use i386 qemu

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-qemu-system-x86_64  -m 5 -boot d -kernel bin/kernel.elf -serial file:Qemu_log.log -no-reboot
+qemu-system-i386  -m 5 -boot d -kernel bin/kernel.elf -serial file:Qemu_log.log -no-reboot
 
 pause
