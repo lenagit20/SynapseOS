ac8a2b8e61030b9ba1e6d4c821792e28fed0523a
Aren Elchinyn <slizen.can@gmail.com>
2021-11-26 22:47:34 +0300

    Add tty to build

--- build.bat
+++ build.bat
@@ -12,12 +12,16 @@ SET LDFLAGS=%CCFLAGS% -nostdlib -lgcc
 :: 
 RMDIR "bin" /S /Q
 mkdir bin
+cd bin
+mkdir io
+cd ..
 
 ::
 echo Using %CCFLAGS%
 %CC% %CCFLAGS% -c kernel/kernel.c -o bin/kernel.o
+%CC% %CCFLAGS% -c io/tty.c -o bin/io/tty.o
 %CC% %CCFLAGS% -c x86/starter.s -o bin/starter.o
-%CC% %LDFLAGS% -T x86/link.ld -o bin/kernel.elf bin/kernel.o bin/starter.o
+%CC% %LDFLAGS% -T x86/link.ld -o bin/kernel.elf bin/kernel.o bin/starter.o bin/io/tty.o
 
 
 pause
