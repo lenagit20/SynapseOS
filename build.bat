dbc53054fc9eab0fffe1d09c31a3d9eb618aa111
Aren Elchinyn <slizen.can@gmail.com>
2021-11-27 13:49:51 +0300

    Create arch folder

--- build.bat
+++ build.bat
@@ -6,8 +6,11 @@ SET AS=i686-elf-as
 SET CC=i686-elf-gcc
 SET LD=i686-elf-ld
 
-SET CCFLAGS=-g -I include -ffreestanding -Wall -Wextra -O2
+SET CCFLAGS=-g -I include -ffreestanding -Wall -Wextra
 SET LDFLAGS=%CCFLAGS% -nostdlib -lgcc
+
+set OBJECTS=bin/kernel.o bin/starter.o bin/io/tty.o bin/libc/string.o
+SET ARCH=arch/x86
 
 :: 
 RMDIR "bin" /S /Q
@@ -20,10 +23,10 @@ cd ..
 ::
 echo Using %CCFLAGS%
 %CC% %CCFLAGS% -c kernel/kernel.c -o bin/kernel.o
-%CC% %CCFLAGS% -c io/tty.c -o bin/io/tty.o
+%CC% %CCFLAGS% -c modules/io/tty.c -o bin/io/tty.o
 %CC% %CCFLAGS% -c libc/string.c -o bin/libc/string.o
-%CC% %CCFLAGS% -c x86/starter.s -o bin/starter.o
-%CC% %LDFLAGS% -T x86/link.ld -o bin/kernel.elf bin/kernel.o bin/starter.o bin/io/tty.o bin/libc/string.o
+%CC% %CCFLAGS% -c %ARCH%/starter.s -o bin/starter.o
+%CC% %LDFLAGS% -T %ARCH%/link.ld -o bin/kernel.elf %OBJECTS%
 
 
 pause
