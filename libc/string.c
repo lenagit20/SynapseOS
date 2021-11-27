59c03a0d3bee20f425c0d7806c7e6cd608ffc485
Aren Elchinyn <slizen.can@gmail.com>
2021-11-27 15:23:04 +0300

    Add memcpy and memset

--- libc/string.c
+++ libc/string.c
@@ -13,3 +13,33 @@ int strlen(const char *str) {
 	}
 	return len;
 }
+
+/*
+    destination - a pointer to the array into which the data will be copied.
+    source - a pointer to an array of the source of the copied data.
+    n - the number of bytes to copy.
+*/
+void *memcpy(void *destination, const void *source, size_t n){
+    char *tmp_dest = (char*)destination;
+    const char *tmp_src = (const char*)source;
+    while (n){
+        *tmp_dest++ = *tmp_src++;
+        n--;
+    }
+    return destination;
+}
+
+
+/*
+    destination - a pointer to the array to fill
+    c - character code to fill
+    n - the size of the filled part of the array in bytes
+*/
+void *memset(void *destination, int c, size_t n){
+    if (destination){
+       char *tmp_mem = (char*)destination;
+       while (n--) *tmp_mem++ = c;
+    }
+    
+    return destination;
+}
