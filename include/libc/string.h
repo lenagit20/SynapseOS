b88faf02cbf6283f1b5f9422476a2aa2c058c2fd
Aren Elchinyn <slizen.can@gmail.com>
2021-11-27 12:39:42 +0300

    Create sting.h

---
+++ include/libc/string.h
@@ -0,0 +1,12 @@
+/*
+    Copyright (C) Aren Elchinyan 2021. All rights reserved.
+    Distributed under terms of the Creative Commons Zero v1.0 Universal License.
+*/
+
+#pragma once
+
+
+#include <kernel.h>
+
+
+int strlen(const char *str);
