03ee76c1172fde61d97d00f2b9997f4f6cfecec7
Aren Elchinyn <slizen.can@gmail.com>
2021-11-30 17:32:05 +0300

    Add documentation

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,7 +5,9 @@
 
 #include <kernel.h>
 
-
+/*
+    kernel_main is the main function of the kernel. It initializes the core modules and the main cycle has been declared.
+*/
 void kernel_main() {
     clean_screen();
     puts("SynapseOS 0.0.1");
