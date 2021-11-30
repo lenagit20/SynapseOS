acabf69580945b772616a121d7f94d2155963df9
Aren Elchinyn <slizen.can@gmail.com>
2021-11-30 17:29:46 +0300

    Add documentation

--- arch/x86/io/ports.c
+++ arch/x86/io/ports.c
@@ -6,6 +6,13 @@
 #include <io/ports.h>
 
 
+/*
+    port_outb is a feature shell for the Assembly instruction with the same name used,
+    to communicate with devices associated with ports.
+
+    uint16_t port - port
+    val - value
+*/
 void port_outb(uint16_t port, uint8_t  val){
     asm volatile( "outb %0, %1" : : "a"(val), "Nd"(port) );
 }
