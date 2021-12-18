dbb0bc976d704797d50e135636e897f51c4fced0
Aren Elchinyn <slizen.can@gmail.com>
2021-12-18 16:01:02 +0300

    Update comments

--- include/io/ports.h
+++ include/io/ports.h
@@ -9,5 +9,5 @@
 #include <stdint.h>
 
 
-void port_outb(uint16_t port, uint8_t  val);
-uint8_t port_inb(uint16_t port);
+void port_outb(uint16_t port, uint8_t  val);    // Wrapper for outb 
+uint8_t port_inb(uint16_t port);                // Wrapper for inb 
