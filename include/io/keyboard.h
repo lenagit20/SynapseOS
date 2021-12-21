723f64fb7992fd3f7421ccb1196890012197d321
Aren Elchinyn <slizen.can@gmail.com>
2021-12-21 21:35:30 +0300

    Create keyboard.h

---
+++ include/io/keyboard.h
@@ -0,0 +1,21 @@
+/*
+    Copyright (C) Aren Elchinyan 2021. All rights reserved.
+    Distributed under terms of the Creative Commons Zero v1.0 Universal License.
+*/
+
+
+#pragma once
+
+
+
+#include <stdint.h>
+#include <stddef.h>
+#include <libc/string.h>
+#include <io/ports.h>
+
+void keyboard_handler();
+void keyboard_init();    // Init keyboard function
+void keyboard_handler_main();    // Main keyboard function
+char getc();                     // Get an a Char from keyboard
+char *gets();                    // Get an a String from keyboard
+
