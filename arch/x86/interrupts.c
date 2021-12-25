fba78a661d6304bd9601ad23a4d4f7a79a27ad9b
Aren Elchinyn <slizen.can@gmail.com>
2021-12-25 17:54:23 +0300

    Recreating modules

--- arch/x86/interrupts.c
+++ arch/x86/interrupts.c
@@ -4,41 +4,18 @@
     Distributed under terms of the Creative Commons Zero v1.0 Universal License.
 */
 
+
 #include <interrupts.h>
 
 
 
-IntDesc *idt = (void*)0xFFFFC000;
 void timer_int_handler();
 
 
 /*
-    init_interrupts used for 
+    
 */
 void init_interrupts() {
-    *((size_t*)0xFFFFEFF0) = 0x8000 | 3;
-
-    memset(idt, 0, 256 * sizeof(IntDesc));
-
-    IDTR idtr = {256 * sizeof(IntDesc), idt};
-
-    asm("lidt (,%0,)"::"a"(&idtr));
-
-    irq_base = 0x20;
-    irq_count = 16;
-
-    port_outb(0x20, 0x11);
-    port_outb(0x21, irq_base);
-    port_outb(0x21, 4);
-    port_outb(0x21, 1);
-    port_outb(0xA0, 0x11);
-    port_outb(0xA1, irq_base + 8);
-    port_outb(0xA1, 2);
-    port_outb(0xA1, 1);
-
-    set_int_handler(irq_base, timer_int_handler, 0x8E);
-
-    asm("sti");
 }
 
 
@@ -46,13 +23,6 @@ void init_interrupts() {
 
 */
 void set_int_handler(uint8_t index, void *handler, uint8_t type) {
-    asm("pushf \n cli");
-    idt[index].selector = 8;
-    idt[index].address_0_15 = (size_t)handler & 0xFFFF;
-    idt[index].address_16_31 = (size_t)handler >> 16;
-    idt[index].type = type;
-    idt[index].reserved = 0;
-    asm("popf");
 }
 
 
@@ -60,5 +30,4 @@ void set_int_handler(uint8_t index, void *handler, uint8_t type) {
 
 */
 IRQ_HANDLER(timer_int_handler) {
-    (*((char*)(0xC03FF000 + 79 * 2)))++;
 }
--- include/interrupts.h
+++ include/interrupts.h
@@ -14,30 +14,6 @@
 #include <io/ports.h>
 
 
-typedef struct {
-    uint16_t address_0_15;
-    uint16_t selector;
-    uint8_t reserved;
-    uint8_t type;
-    uint16_t address_16_31;
-} __attribute__((packed)) IntDesc;
-
-
-typedef struct {
-    uint16_t limit;
-    void *base;
-} __attribute__((packed)) IDTR;
-
-
-uint8_t irq_base;
-uint8_t irq_count;
-
-
 void timer_int_handler();
 void init_interrupts();
 void set_int_handler(uint8_t index, void *handler, uint8_t type);
-
-
-#define IRQ_HANDLER(name) void name(); \
-    asm(#name ": pusha \n call _" #name " \n movb $0x20, %al \n outb %al, $0x20 \n outb %al, $0xA0 \n popa \n iret"); \
-    void _ ## name()
