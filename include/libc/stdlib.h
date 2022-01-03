68b245f83047595ec110485646313df72c5af9cc
Aren Elchinyn <slizen.can@gmail.com>
2022-01-03 14:50:40 +0300

    Create stdlib.h

---
+++ include/libc/stdlib.h
@@ -0,0 +1,13 @@
+/*
+    Copyright (C) Aren Elchinyan 2021. All rights reserved.
+    Distributed under terms of the Creative Commons Zero v1.0 Universal License.
+*/
+
+#pragma once
+
+
+#include <stddef.h>
+
+
+void reverse(char s[]);
+void itoa(int n, char s[]);
