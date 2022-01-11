9603e56ecdc284f109ef10d1621a760f8f8150a6
Aren Elchinyn <slizen.can@gmail.com>
2022-01-11 18:07:29 +0300

    Fix codestyle

--- libc/stdlib.c
+++ libc/stdlib.c
@@ -3,15 +3,16 @@
     Distributed under terms of the Creative Commons Zero v1.0 Universal License.
 */
 
+
 #include <libc/stdlib.h>
 #include <libc/string.h>
 
-void strver(char *str)
-{
+
+void strver(char *str) {
     char c;
     int j = strlen(str) - 1;
-    for (int i = 0; i < j; i++)
-    {
+
+    for (int i = 0; i < j; i++) {
         c = str[i];
         str[i] = str[j];
         str[j] = c;
@@ -19,20 +20,22 @@ void strver(char *str)
     }
 }
 
-int itoa(int n, char *buffer)
-{
+
+int itoa(int n, char *buffer) {
     int length = n < 0;
-    if (length)
-    {
+    
+    if (length) {
         n = -n;
         *buffer = '-';
     }
-    do
-    {
+    
+    do {
         buffer[length++] = (n % 10) + '0';
         n /= 10;
     } while (n > 0);
+
     buffer[length] = '\0';
     strver(buffer);
+    
     return length;
 }
