5f82ef400bafc17ace984af9117877d09851d81d
Aren Elchinyn <slizen.can@gmail.com>
2022-01-15 12:18:32 +0300

    Add debug

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
-@echo qemu-system-i386  -m 5 -boot d -kernel bin/kernel.elf -serial file:Qemu_log.log -no-reboot
+@echo qemu-system-i386 -d int -m 5 -boot d -kernel bin/kernel.elf -serial file:Qemu_log.log -no-reboot
 
 
-@qemu-system-i386  -m 5 -boot d -kernel bin/kernel.elf -serial file:Qemu_log.log -no-reboot
+@qemu-system-i386 -d int -m 5 -boot d -kernel bin/kernel.elf -serial file:Qemu_log.log -no-reboot
 
 @pause
