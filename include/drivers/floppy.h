966095f4bf2f274699dbd10b1990e2fe19084988
Aren Elchinyn <slizen.can@gmail.com>
2022-02-07 22:32:29 +0300

    Update floppy.h

--- include/drivers/floppy.h
+++ include/drivers/floppy.h
@@ -8,5 +8,62 @@
 
 
 #include <stdint.h>
-#include <io/tty.h>>
+#include <io/tty.h>
 #include <io/ports.h>
+
+
+
+void floppy_detect_drives();
+void test_floppy();
+
+
+
+enum FloppyCommands
+{
+   READ_TRACK =                 2,	// generates IRQ6
+   SPECIFY =                    3,      // * set drive parameters
+   SENSE_DRIVE_STATUS =         4,
+   WRITE_DATA =                 5,      // * write to the disk
+   READ_DATA =                  6,      // * read from the disk
+   RECALIBRATE =                7,      // * seek to cylinder 0
+   SENSE_INTERRUPT =            8,      // * ack IRQ6, get status of last command
+   WRITE_DELETED_DATA =         9,
+   READ_ID =                    10,	// generates IRQ6
+   READ_DELETED_DATA =          12,
+   FORMAT_TRACK =               13,     // *
+   DUMPREG =                    14,
+   SEEK =                       15,     // * seek both heads to cylinder X
+   VERSION =                    16,	// * used during initialization, once
+   SCAN_EQUAL =                 17,
+   PERPENDICULAR_MODE =         18,	// * used during initialization, once, maybe
+   CONFIGURE =                  19,     // * set controller parameters
+   LOCK =                       20,     // * protect controller params from a reset
+   VERIFY =                     22,
+   SCAN_LOW_OR_EQUAL =          25,
+   SCAN_HIGH_OR_EQUAL =         29
+};
+
+
+enum FloppyRegisters {
+   STATUS_REGISTER_A                = 0x3F0, // read-only
+   STATUS_REGISTER_B                = 0x3F1, // read-only
+   DIGITAL_OUTPUT_REGISTER          = 0x3F2,
+   TAPE_DRIVE_REGISTER              = 0x3F3,
+   MAIN_STATUS_REGISTER             = 0x3F4, // read-only
+   DATARATE_SELECT_REGISTER         = 0x3F4, // write-only
+   DATA_FIFO                        = 0x3F5,
+   DIGITAL_INPUT_REGISTER           = 0x3F7, // read-only
+   CONFIGURATION_CONTROL_REGISTER   = 0x3F7  // write-only
+};
+
+static const char * drive_types[8] = {
+    "none",
+    "360kB 5.25\"",
+    "1.2MB 5.25\"",
+    "720kB 3.5\"",
+
+    "1.44MB 3.5\"",
+    "2.88MB 3.5\"",
+    "unknown type",
+    "unknown type"
+};
