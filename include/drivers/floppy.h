482f7063a8960fe88a0e46fcd20222cac3d17b36
Aren Elchinyn <slizen.can@gmail.com>
2022-02-07 21:56:06 +0300

    Create floppy.h

---
+++ include/drivers/floppy.h
@@ -0,0 +1,12 @@
+/*
+    Copyright (C) Aren Elchinyan 2021-2022. All rights reserved.
+    Distributed under terms of the Creative Commons Zero v1.0 Universal License.
+*/
+
+
+#pragma once
+
+
+#include <stdint.h>
+#include <io/tty.h>>
+#include <io/ports.h>
