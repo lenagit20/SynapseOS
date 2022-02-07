93803eaad9590e43c28b88bd3dd32bcd526aa145
Aren Elchinyn <slizen.can@gmail.com>
2022-02-07 18:09:16 +0300

    Update keyboard.h

--- include/io/keyboard.h
+++ include/io/keyboard.h
@@ -11,8 +11,16 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <libk/string.h>
+#include <io/tty.h>
 #include <io/ports.h>
 
+
+#define KEYBOARD_DATA_PORT 0x60
+#define KEYBOARD_STATUS_PORT 0x64
+
+#define ENTER_KEY_CODE 0x1C
+
+extern unsigned char keyboard_map[128];
 
 void keyboard_handler();
 void keyboard_init();    // Init keyboard function
@@ -20,9 +28,7 @@ void keyboard_handler_main();    // Main keyboard function
 char getc();                     // Get an a Char from keyboard
 char *gets();                    // Get an a String from keyboard
 
-void keyboard_handler(/* struct regs *r */) {
-    asm("hlt");
-}
+
 
 
 char keyboard_scancodes[] = {
