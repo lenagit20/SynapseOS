7f29abac7bf7f3f924992cab2f5e86b49788d8e1
Aren Elchinyn <slizen.can@gmail.com>
2022-02-07 18:53:25 +0300

    try fix keyboard

--- include/io/keyboard.h
+++ include/io/keyboard.h
@@ -4,55 +4,14 @@
 */
 
 
-#pragma once
-
-
 
 #include <stdint.h>
 #include <stddef.h>
 #include <libk/string.h>
 #include <io/tty.h>
 #include <io/ports.h>
-
-unsigned char keyboard_map[128] =
-{
-    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
-  '9', '0', '-', '=', '\b',	/* Backspace */
-  '\t',			/* Tab */
-  'q', 'w', 'e', 'r',	/* 19 */
-  't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
-    0,			/* 29   - Control */
-  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
- '\'', '`',   0,		/* Left shift */
- '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
-  'm', ',', '.', '/',   0,				/* Right shift */
-  '*',
-    0,	/* Alt */
-  ' ',	/* Space bar */
-    0,	/* Caps lock */
-    0,	/* 59 - F1 key ... > */
-    0,   0,   0,   0,   0,   0,   0,   0,
-    0,	/* < ... F10 */
-    0,	/* 69 - Num lock*/
-    0,	/* Scroll Lock */
-    0,	/* Home key */
-    0,	/* Up Arrow */
-    0,	/* Page Up */
-  '-',
-    0,	/* Left Arrow */
-    0,
-    0,	/* Right Arrow */
-  '+',
-    0,	/* 79 - End key*/
-    0,	/* Down Arrow */
-    0,	/* Page Down */
-    0,	/* Insert Key */
-    0,	/* Delete Key */
-    0,   0,   0,
-    0,	/* F11 Key */
-    0,	/* F12 Key */
-    0,	/* All other keys are undefined */
-};
+#ifndef KBD_H
+#define KBD_H
 
 #define KEYBOARD_DATA_PORT 0x60
 #define KEYBOARD_STATUS_PORT 0x64
@@ -61,15 +20,8 @@ unsigned char keyboard_map[128] =
 
 extern unsigned char keyboard_map[128];
 
-void keyboard_handler();
-void keyboard_init();    // Init keyboard function
-void keyboard_handler_main();    // Main keyboard function
-char getc();                     // Get an a Char from keyboard
-char *gets();                    // Get an a String from keyboard
+void kb_init(void);
+void keyboard_handler(void);
+void keyboard_handler_main(void);
 
-
-
-
-char keyboard_scancodes[] = {
-    '0'
-};
+#endif
