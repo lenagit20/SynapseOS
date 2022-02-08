031ff43b65ebb56a3f99918338aff91ff7e7fecd
Aren Elchinyn <slizen.can@gmail.com>
2022-02-08 06:36:01 +0300

    Create shell.c

---
+++ modules/shell.c
@@ -0,0 +1,11 @@
+/*
+    Copyright (C) Aren Elchinyan 2021-2022. All rights reserved.
+    Distributed under terms of the Creative Commons Zero v1.0 Universal License.
+*/
+
+#include <lang0x42.h>
+#include <shell.h.h>
+
+
+void shell(char *input){
+}
