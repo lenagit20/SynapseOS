c47b4a626cc5e7390b9f615a4a8a605e35746802
Aren Elchinyn <slizen.can@gmail.com>
2022-02-08 05:59:31 +0300

    Update floppy.c

--- drivers/floppy.c
+++ drivers/floppy.c
@@ -11,8 +11,11 @@ void floppy_detect_drives() {
    port_outb(0x70, 0x10);
    unsigned drives = port_inb(0x71);
 
-   putsln(" - Floppy drive 0: %s\n", drive_types[drives >> 4]);
-   putsln(" - Floppy drive 1: %s\n", drive_types[drives & 0xf]);
+   putsln("Floppy drives:");
+   puts(" - Floppy drive 0: ");
+   putsln(drive_types[drives >> 4]);
+   puts(" - Floppy drive 1: ");
+   putsln(drive_types[drives & 0xf]);
 
 }
 
