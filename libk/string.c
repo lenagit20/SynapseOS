d5e05aef935a413a581a030e140c51674a5b6864
Aren Elchinyn <slizen.can@gmail.com>
2022-02-08 20:50:58 +0300

    Add strcmp

--- libk/string.c
+++ libk/string.c
@@ -56,3 +56,23 @@ void *memset(void *destination, int c, size_t n){
     
     return destination;
 }
+
+
+/*
+    strcmp is needed for string comparison
+
+    str1, str2 - strings to be compared
+*/
+int strcmp(const char *str1, const char *str2) {
+    int s1;
+    int s2;
+
+    do {
+        s1 = *str1++;
+        s2 = *str2++;
+        if (s1 == 0)
+            break;
+    } while (s1 == s2);
+
+    return (s1 < s2) ? -1 : (s1 > s2);
+}
