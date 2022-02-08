83ab22c4d8b78fca95eb1ca27b1ac9ca31badf62
Aren Elchinyn <slizen.can@gmail.com>
2022-02-08 05:48:36 +0300

    Update keyboard.c

--- modules/io/keyboard.c
+++ modules/io/keyboard.c
@@ -18,43 +18,92 @@ int input_type;
 char keycode; 
 
 
-unsigned char keyboard_map[128] = {
-    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
-  '9', '0', '-', '=', '\b',	/* Backspace */
-  '\t',			/* Tab */
-  'q', 'w', 'e', 'r',	/* 19 */
-  't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
-    0,			/* 29   - Control */
-  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
- '\'', '`',   0,		/* Left shift */
- '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
-  'm', ',', '.', '/',   0,				/* Right shift */
-  '*',
-    0,	/* Alt */
-  ' ',	/* Space bar */
-    0,	/* Caps lock */
-    0,	/* 59 - F1 key ... > */
-    0,   0,   0,   0,   0,   0,   0,   0,
-    0,	/* < ... F10 */
-    0,	/* 69 - Num lock*/
-    0,	/* Scroll Lock */
-    0,	/* Home key */
-    0,	/* Up Arrow */
-    0,	/* Page Up */
-  '-',
-    0,	/* Left Arrow */
+unsigned  char keyboard_map[] = {
+    0, 
+    27, //`
+    '1', '2',  '3', '4',  '5', '6',  '7', '8',	'9',  '0', 
+    '-', '=',  
+    '\b',	/*  Backspace  */
+    '\t',			/*  Tab  */
+    'q', 'w',  'e', 'r',	/*  19 */
+    't', 'y',  'u', 'i',  'o', 'p',  '[', ']',  '\n',	/*  Enter  key  */
+    0,			/*  29 - Control */
+    'a', 's',  'd', 'f',  'g', 'h',  'j', 'k',  'l', ';',	/*  39 */
+    '\'',  '`', 
+    -2,		/*  Left shift 42 */
+    '\\',  'z', 'x',  'c', 'v',  'b', 'n',			/*  49 */
+    'm', ',',  '.', '/',  
+    -2,				/*  Right  shift  54*/
+    '*',
+    0,	/*  Alt  */
+    ' ',	/*  Space  bar  */
+    -1,	/*  Caps lock  */
+    0,	/*  59 - F1  key  ...  >  */
+    0, 0,  0, 0,  0, 0,  0, 0,
+    0,	/*  <  ...  F10  */
+    0,	/*  69 - Num lock*/
+    0,	/*  Scroll Lock  */
+    0,	/*  Home key */
+    0,	/*  Up Arrow */
+    0,	/*  Page Up  */
+    '-',
+    0,	/*  Left Arrow */
     0,
-    0,	/* Right Arrow */
-  '+',
-    0,	/* 79 - End key*/
-    0,	/* Down Arrow */
-    0,	/* Page Down */
-    0,	/* Insert Key */
-    0,	/* Delete Key */
-    0,   0,   0,
-    0,	/* F11 Key */
-    0,	/* F12 Key */
-    0,	/* All other keys are undefined */
+    0,	/*  Right  Arrow  */
+    '+',
+    0,	/*  79 - End key*/
+    0,	/*  Down Arrow */
+    0,	/*  Page Down  */
+    0,	/*  Insert Key */
+    0,	/*  Delete Key */
+    0, 0,  0,
+    0,	/*  F11  Key  */
+    0,	/*  F12  Key  */
+    0,	/*  All  other  keys are undefined */
+};
+
+unsigned  char keyboard_map_shifted[] = {
+    0, 
+    27, //`
+    '!', '@',  '#', '$',  '%', '^',  '&', '*',	'(',  ')', 
+    '_', '+',  
+    '\b',	/*  Backspace  */
+    '\t',			/*  Tab  */
+    'Q', 'W',  'E', 'R',	/*  19 */
+    'T', 'Y',  'U', 'I',  'O', 'P',  '{', '}',  '\n',	/*  Enter  key  */
+    0,			/*  29 - Control */
+    'A', 'S',  'D', 'F',  'G', 'H',  'J', 'K',  'L', ':',	/*  39 */
+    '"',  '`', 
+    1,		/*  Left shift */
+    '|',  'Z', 'X',  'C', 'V',  'B', 'N',			/*  49 */
+    'M', '<',  '>', '?',  
+    1,				/*  Right  shift  */
+    '*',
+    0,	/*  Alt  */
+    ' ',	/*  Space  bar  */
+    -1,	/*  Caps lock  */
+    0,	/*  59 - F1  key  ...  >  */
+    0, 0,  0, 0,  0, 0,  0, 0,
+    0,	/*  <  ...  F10  */
+    0,	/*  69 - Num lock*/
+    0,	/*  Scroll Lock  */
+    0,	/*  Home key */
+    0,	/*  Up Arrow */
+    0,	/*  Page Up  */
+    '-',
+    0,	/*  Left Arrow */
+    0,
+    0,	/*  Right  Arrow  */
+    '+',
+    0,	/*  79 - End key*/
+    0,	/*  Down Arrow */
+    0,	/*  Page Down  */
+    0,	/*  Insert Key */
+    0,	/*  Delete Key */
+    0, 0,  0,
+    0,	/*  F11  Key  */
+    0,	/*  F12  Key  */
+    0,	/*  All  other  keys are undefined */
 };
 
 void kb_init(void) {
@@ -81,6 +130,10 @@ void keyboard_handler_main(void) {
 		
 		if(keycode < 0)
 			return;
+    
+    if (keycode == -86){
+      SHIFT = 1;
+    }
     if (input_type == 2){
       log_puts("getch: ");
       itoa(keycode, res);
@@ -99,12 +152,16 @@ void keyboard_handler_main(void) {
 			putsln("EXIT");
 			return;
 		}
+    
 
 		log_puts("Scancode: ");
 		itoa(keycode, res);
 		log_putsln(res);
-
-		putchar(keyboard_map[(unsigned char) keycode]);
+    if (SHIFT == 0){
+			putchar(keyboard_map[(unsigned char) keycode]);
+		} else {
+      putchar(keyboard_map_shifted[(unsigned char) keycode]);
+		}
     	//qemu_printf("key = %c (index %d)\n", keyboard_map[(unsigned char) keycode], (unsigned char) keycode);
 	}
 }
