fb0f286a4ed1d6ead4929759b6d8bab5a34c8650
Aren Elchinyn <slizen.can@gmail.com>
2022-02-10 21:31:16 +0300

    Update shell.c

--- modules/shell.c
+++ modules/shell.c
@@ -3,9 +3,12 @@
     Distributed under terms of the Creative Commons Zero v1.0 Universal License.
 */
 
+
 #include <lang0x42.h>
 #include <shell.h>
 
 
 void shell(char *input){
+    log_putsln("SHELL:");
+    log_putsln(input);
 }
