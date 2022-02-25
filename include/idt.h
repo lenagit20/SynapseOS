2af55ccc8a23ae4559b7536f251455c8abdc9a39
Aren Elchinyn <slizen.can@gmail.com>
2022-02-25 17:08:06 +0300

    Update idt.h

--- include/idt.h
+++ include/idt.h
@@ -12,6 +12,14 @@
 
 #define IDT_SIZE 256
 #define INTERRUPT_GATE 0x8e
+#define PIC1		0x20		/* IO base address for master PIC */
+#define PIC2		0xA0		/* IO base address for slave PIC */
+#define PIC_EOI		0x2
+#define PIC1_COMMAND	PIC1
+#define PIC1_DATA	(PIC1+1)
+#define PIC2_COMMAND	PIC2
+#define PIC2_DATA	(PIC2+1)
+
 
 
 struct IDT_entry {
@@ -24,3 +32,5 @@ struct IDT_entry {
 
 
 void idt_init();
+void disable_pic();
+void init_pic();
