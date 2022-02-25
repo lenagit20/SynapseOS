431f0e9aba327753d7c807053d9913da364d150d
Aren Elchinyn <slizen.can@gmail.com>
2022-02-25 20:10:11 +0300

    Add PIC_remap

--- arch/x86/pic.c
+++ arch/x86/pic.c
@@ -9,7 +9,39 @@
 #include <io/ports.h>
 #include <io/keyboard.h>
 #include <exceptions.h>
+ 
 
+/*
+	offset1 - vector offset for master PIC
+	offset2 - same for slave PIC: offset2..offset2+7
+*/
+void PIC_remap(int offset1, int offset2) {
+	unsigned char a1, a2;
+ 
+	a1 = port_inb(PIC1_DATA);                        // save masks
+	a2 = port_inb(PIC2_DATA);
+ 
+	port_outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);  // starts the initialization sequence (in cascade mode)
+	io_wait();
+	port_outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+	io_wait();
+	port_outb(PIC1_DATA, offset1);                 // ICW2: Master PIC vector offset
+	io_wait();
+	port_outb(PIC2_DATA, offset2);                 // ICW2: Slave PIC vector offset
+	io_wait();
+	port_outb(PIC1_DATA, 4);                       // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
+	io_wait();
+	port_outb(PIC2_DATA, 2);                       // ICW3: tell Slave PIC its cascade identity (0000 0010)
+	io_wait();
+ 
+	port_outb(PIC1_DATA, ICW4_8086);
+	io_wait();
+	port_outb(PIC2_DATA, ICW4_8086);
+	io_wait();
+ 
+	port_outb(PIC1_DATA, a1);   // restore saved masks.
+	port_outb(PIC2_DATA, a2);
+}
 
 /*
     disable_pic - is used to disable the PIC
